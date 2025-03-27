578322c483d4bf407b91691017e886f712aff25e
TM <tonnyma@gmail.com>
2025-03-28 01:12:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { supabase } from '@/lib/supabase';
 
 
 const Gallery = () => {
-  const [portfolios, setPortfolios] = useState<any[]>([]);
+  const [portfolios, setPortfolios] = useState<{ id: number; name: string; url: string }[]>([]);
   const [name, setName] = useState('');
   const [url, setUrl] = useState('');
   const [email, setEmail] = useState(''); // State for email
